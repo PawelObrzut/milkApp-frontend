e8d4723e2c806b9893c3177a622318c14f6d5a7f
PawelObrzut <pawel.r.obrzut@gmail.com>
2023-01-24 22:55:16 +0100

    sketches draft for a dropdown filter

--- src/components/Nav/Nav.tsx
+++ src/components/Nav/Nav.tsx
@@ -8,6 +8,33 @@ const Nav = () => {
         <BsSearch className="nav--search__icon"/>
         <input className="nav--search__input" type="text" placeholder="Search"></input>
       </div>
+
+      <div className="nav--filter"> 
+        <p className="nav--filter__title">Filter</p>
+        <ul className="nav--filter__dropdown active">
+        <p className="nav--filter__category">Milk type</p>
+          <li>
+            <label>
+              <input type="checkbox" />
+              Whole Milk
+            </label>
+          </li>
+          <li>
+            <label>
+              <input type="checkbox" />
+              Oat Milk
+            </label>
+          </li>
+          <li>
+            <label>
+              <input type="checkbox" />
+              Almond Milk
+            </label>
+          </li>
+        </ul>
+      </div>
+
+
     </nav>
   )
 }
