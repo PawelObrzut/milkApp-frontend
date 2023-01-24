54a8339b71594b34aeaf3298514b6c43c1c16016
PawelObrzut <pawel.r.obrzut@gmail.com>
2023-01-24 13:14:48 +0100

    sets colors and a font

---
+++ src/components/PageTitle.tsx
@@ -0,0 +1,11 @@
+import './PageTitle.css'
+
+const PageTitle = () => {
+  return (
+    <header className="header">
+      <h1 className="header--title">the milk store</h1>
+    </header>
+  )
+}
+
+export default PageTitle
