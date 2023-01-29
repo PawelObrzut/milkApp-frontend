9630e6ec574f57d78d71e1746fdfcc121da35a1f
PawelObrzut <pawel.r.obrzut@gmail.com>
2023-01-29 15:52:50 +0100

    uses https protocole

--- src/context/MilkContext.tsx
+++ src/context/MilkContext.tsx
@@ -34,10 +34,10 @@ export const MilkProvider = ({ children }: MilkProviderProps) => {
   const getMilksPage = (page: string | undefined, typesFilter?: string[]):void => {
     let url = '';
     if (typesFilter === undefined) {
-      url = `http://backend-dev.eu-north-1.elasticbeanstalk.com/api/milks?page=${page}&limit=9`;
+      url = `https://backend-dev.eu-north-1.elasticbeanstalk.com/api/milks?page=${page}&limit=9`;
     } else {
       const searchUrlFormat = typesFilter.join('%2B').replace(/ /g, '%20');
-      url = `http://backend-dev.eu-north-1.elasticbeanstalk.com/api/milks?page=${page}&limit=9&filter=${searchUrlFormat}`;
+      url = `https://backend-dev.eu-north-1.elasticbeanstalk.com/api/milks?page=${page}&limit=9&filter=${searchUrlFormat}`;
     }
 
     fetch(url, {
